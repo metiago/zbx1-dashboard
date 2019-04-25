d64ae727c010c0fd01585ca38478bd5f1a9545a3
tiago <ctw00244@criticaltechworks.com>
2019-04-25 15:55:35 +0100

    Added logout func to clean token store

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
 
-import { login } from '../../utils/AuthService';
+import { login, logout } from '../../utils/AuthService';
 import Profile from '../../dashboard/Profile';
 import Input from '../../components/input/Input'
 import { validationError } from '../../utils/Request';
@@ -29,6 +29,10 @@ class Login extends Component {
 
   onChangePassword(event) {
     this.setState({ password: event.target.value });
+  }
+
+  componentDidMount() {
+    logout()
   }
 
   validateForm() {
